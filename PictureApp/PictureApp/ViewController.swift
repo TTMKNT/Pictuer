8bdb77bda2bdbabcd919f6b7bbac6270690a3976
くら〜〜く <masa812n@gmail.com>
2018-04-29 16:39:05 +0900

    no message

--- PictureApp/PictureApp/ViewController.swift
+++ PictureApp/PictureApp/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  PictureApp
+//  tempPicture
 //
-//  Created by くら〜〜く on 2018/04/29.
+//  Created by くら〜〜く on 2018/04/27.
 //  Copyright © 2018年 くら〜〜く. All rights reserved.
 //
 
